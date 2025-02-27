c7e0c09f0b7a42b9c58f2832a8c5e95850c5c49d
abcdefghij803 <analtamash1@gmail.com>
2025-02-28 01:43:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,7 +5,7 @@ class Config(object):
     # Pyrogram Client
     API_ID = int(os.environ.get("API_ID", "16838432"))  # ⚠️ Required
     API_HASH = os.environ.get("API_HASH", "866e06e7d4a7ab8bd1eb0f770242b878")  # ⚠️ Required
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7707056569:AAHVSn6tXkq4nAbBiFOTOtp3eqN4wNgQcWs")  # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7707056569:AAHPI-Tw4N7EqGHkq1hTsIdWV1P8QObvHDM")  # ⚠️ Required
     
     # Session Strings for Multiple Accounts
     ACCOUNTS = [
